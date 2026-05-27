ebe17f231b361a66f7bbaac7da968879596ec654
ThIagoMedeiros21 <thfmadeiros01@gmail.com>
2026-05-27 13:54:33 -0300

    repositorys on

---
+++ backend/app/repository/log_repository.py
@@ -0,0 +1,49 @@
+from sqlalchemy.orm import Session
+from app.models.survey import LogAB
+from app.enums import RespondentType
+
+def create_log(db:Session, survey_id: int, session_id: str, respondent_type: RespondentType):
+    log = LogAB(survey_id = survey_id, session_id = session_id, respondent_type = respondent_type)
+    db.add(log)
+    db.commit()
+    db.refresh(log)
+    return log
+
+def get_log(db: Session, id: int):
+    return db.query(LogAB).filter_by(id=id).first()
+
+def get_all_log(db: Session):
+    return db.query(LogAB).all()
+
+def increment_access(db: Session, id: int):
+    found = db.query(LogAB).filter_by(id=id).first()
+    if not found:
+        return None
+    found.accesses += 1
+    db.commit()
+    return found
+
+def increment_dropout(db: Session, id: int):
+    found = db.query(LogAB).filter_by(id=id).first()
+    if not found:
+        return None
+    found.dropouts += 1
+    db.commit()
+    return found
+
+def increment_accessibility_interaction(db: Session, id: int):
+    found = db.query(LogAB).filter_by(id=id).first()
+    if not found:
+        return None
+    found.accessibility_interactions += 1
+    db.commit()
+    return found
+
+def delete_log(db: Session, id: int):
+    found = db.query(LogAB).filter_by(id=id).first()
+    if not found:
+        return None
+    db.delete(found)
+    db.commit()
+    return found
+
