380ad6b5eb7c1fcc8be2e0a1dcbc3f49fc378fa1
ThIagoMedeiros21 <thfmadeiros01@gmail.com>
2026-05-27 14:18:21 -0300

    fix

--- backend/app/repository/responses_repository.py
+++ backend/app/repository/responses_repository.py
@@ -1,21 +1,21 @@
 from sqlalchemy.orm import Session
-from app.models.survey import Respostas
+from app.models.survey import Resposta
 
 def create_responses(db: Session, survey_id: int, answered_data: dict):
-    responses = Respostas(survey_id = survey_id, answered_data = answered_data)
+    responses = Resposta(survey_id = survey_id, answered_data = answered_data)
     db.add(responses)
     db.commit()
     db.refresh(responses)
     return responses
 
 def get_responses(db: Session, id: int):
-    return db.query(Respostas).filter_by(id=id).first()
+    return db.query(Resposta).filter_by(id=id).first()
 
 def get_all_responses(db: Session):
-    return db.query(Respostas).all()
+    return db.query(Resposta).all()
 
 def delete_responses(db: Session, id: int):
-    responses = db.query(Respostas).filter_by(id=id).first()
+    responses = db.query(Resposta).filter_by(id=id).first()
     if not responses:
         return None
     db.delete(responses)
