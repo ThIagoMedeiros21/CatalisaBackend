102d895f84f0c28d753e5a165f4bc605b6ea2677
ThIagoMedeiros21 <thfmadeiros01@gmail.com>
2026-05-28 15:11:29 -0300

    fix survey routers

--- backend/app/routers/survey_router.py
+++ backend/app/routers/survey_router.py
@@ -15,36 +15,37 @@ def get_all_surveys(db: Session = Depends(get_db)):
 
 @router.get("/{id}")
 def get_survey(id: int, db: Session = Depends(get_db)):
-
+    
     survey = survey_service.get_survey(db, id)
     if survey is None:
         raise HTTPException(status_code=404, detail="Survey não encontrado")
-    
-    else:
-        return survey
-    
+    return survey
+
 @router.delete("/{id}")
 def delete_survey(id: int, db: Session = Depends(get_db)):
     survey = survey_service.get_survey(db, id)
     if survey is None:
         raise HTTPException(status_code=404, detail="Survey não encontrado")
-    else:
-        survey_service.delete_survey(db,id)
-        return Response(status_code=204)
+    
+    resultado = survey_service.delete_survey(db, id)
+    if resultado is None:
+        raise HTTPException(status_code=409, detail="Survey possui respostas ou logs vinculados")
+    return Response(status_code=204)
 
-@router.post("")
+@router.post("", status_code=201)
 def create_survey(data: SurveyCreate, db: Session = Depends(get_db)):
     resultado = survey_service.create_survey(db, data.title, data.respondent_type, data.is_active)
     if resultado is None:
-        raise HTTPException(status_code=409, detail="Survey já existe") 
+        raise HTTPException(status_code=409, detail="Survey já existe")
     return resultado
 
-@router.put("/{id}")#colcoar o patch
-def update_survey(data: SurveyUpdate, id:int, db: Session = Depends(get_db),):
+@router.patch("/{id}")
+def update_survey(data: SurveyUpdate, id: int, db: Session = Depends(get_db)):
     survey = survey_service.get_survey(db, id)
     if survey is None:
         raise HTTPException(status_code=404, detail="Survey não encontrado")
     
-    else:
-        up = survey_service.update_survey(db, id, data.title, data.respondent_type, data.is_active)
-        return up
+    resultado = survey_service.update_survey(db, id, data.title, data.respondent_type, data.is_active)
+    if resultado is None:
+        raise HTTPException(status_code=409, detail="Já existe outro survey com esse título e tipo")
+    return resultado
