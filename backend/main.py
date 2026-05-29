0a3f228ba614950f1cbbcddf65997a3f5c5cc35f
ThIagoMedeiros21 <thfmadeiros01@gmail.com>
2026-05-29 18:59:26 -0300

    fix main.py

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,4 @@ def hello():
 
 app.include_router(survey_router.router, prefix="/surveys", tags=["Surveys"])
 app.include_router(logs_router.router, prefix="/logs", tags=["logs"])
+app.include_router(logs_router.router, prefix="/response", tags=["response"])
