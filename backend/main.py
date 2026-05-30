ea51f80c85ab608ff337a6494704f591e35dd1f5
ThIagoMedeiros21 <thfmadeiros01@gmail.com>
2026-05-30 17:35:15 -0300

    fix main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routers import survey_router
 from app.routers import logs_router
 from app.routers import response_router
-
+from app.routers import submit_router
 app = FastAPI()
 
 @app.get("/")
@@ -12,3 +12,4 @@ def hello():
 app.include_router(survey_router.router, prefix="/surveys", tags=["Surveys"])
 app.include_router(logs_router.router, prefix="/logs", tags=["Logs"])
 app.include_router(response_router.router, prefix="/responses", tags=["Responses"])
+app.include_router(submit_router.router, prefix="/submit", tags=["Submit"])
