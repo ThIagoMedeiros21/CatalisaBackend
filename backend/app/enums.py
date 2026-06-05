23dd2a1649f86f140701b80f4e15183bc05c9a2c
Thiago Fernandes de Medeiros <thfmadeiros01@gmail.com>
2026-06-05 16:38:20 -0300

    Fix enum value casing for co_teacher

--- backend/app/enums.py
+++ backend/app/enums.py
@@ -2,7 +2,7 @@ import enum
 
 class RespondentType(enum.Enum):
     cesar_school_student = "Estudante da CESAR School"
-    Co_teacher = "Cocente/Colaborador"
+    co_teacher = "Cocente/Colaborador"
     exhibiting_student = "Estudante expositor da Mostra TechDesign"
     high_school_student = "Estudante de esino médio(Visitante)"
     teacher = "Docente/colaborador"
