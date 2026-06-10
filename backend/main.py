10cce7f4c9789e5d4f46affc03840d6e90685a1f
ThIagoMedeiros21 <thfmadeiros01@gmail.com>
2026-06-10 02:17:07 -0300

    updt

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://catalisa-frontend-mt.vercel.app/"],
+    allow_origins=["https://catalisa-frontend-mt.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
